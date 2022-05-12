bb71cd480d39a57ce979d44d4f21b12496b47328
Henrique Costa <hvcosta@stefanini.com>
2022-05-12 12:05:01 -0300

    Add project files.

---
+++ Controllers/MovieController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MoviesAPI.Models;
+using System;
+using System.Collections.Generic;
+
+namespace MoviesAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MovieController
+    {
+        private static List<Movie> movies = new List<Movie>();
+
+        [HttpPost]
+        public void Create([FromBody] Movie movie)
+        {
+            movies.Add(movie);
+            Console.WriteLine(movie.Title);
+
+        }
+    }
+}
